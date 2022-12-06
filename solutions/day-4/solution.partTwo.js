e6f1a3df38cf8d7bf88f9efa80c3fc31753f0b8e
Dobromił Cepil <dobromil.cepil@fortedigital.no>
2022-12-06 14:42:25 +0100

    day 4 solutions

---
+++ solutions/day-4/solution.partTwo.js
@@ -0,0 +1,22 @@
+import { lines } from "./input.js";
+
+const sum = lines.reduce((prev, curr) => {
+  const [firstRange, secondRange] = curr.split(",");
+  const [firstStart, firstEnd] = firstRange.split("-");
+  const [secondStart, secondEnd] = secondRange.split("-");
+  const first = [...Array(firstEnd - firstStart + 1).keys()].map(
+    (x) => x + parseInt(firstStart)
+  );
+  const second = [...Array(secondEnd - secondStart + 1).keys()].map(
+    (x) => x + parseInt(secondStart)
+  );
+  if (
+    first.some((v) => second.includes(v)) ||
+    second.some((v) => first.includes(v))
+  ) {
+    prev++;
+  }
+  return prev;
+}, 0);
+
+console.log(sum);
