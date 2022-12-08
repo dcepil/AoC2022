c31846826ba7ac59bf6403a3b33c299aec8dc519
Dobromił Cepil <dobromil.cepil@fortedigital.no>
2022-12-08 15:30:08 +0100

    day 8 wip

--- solutions/day-8/solution.partTwo.js
+++ solutions/day-8/solution.partTwo.js
@@ -7,13 +7,15 @@ const sum = lines.reduce((prev, line, lIdx) => {
       char > Math.max(...line.slice(cIdx + 1, line.length)),
     ];
     console.log(left, char);
+    // if is visible then where is it from the given side
+    // if not visible then what's blocking it and what's the difference in the items indicies
     console.log(line.indexOf(Math.max(...line.slice(0, cIdx))));
     const [up, down] = [
       char > Math.max(...lines.slice(0, lIdx).map((v) => v[cIdx])),
       char >
         Math.max(...lines.slice(lIdx + 1, lines.length).map((v) => v[cIdx])),
     ];
-    return left || right || up || down ? prev++ : prev;
+    // return left || right || up || down ? prev++ : prev;
   });
   return prev;
 }, 0);
